f7f6f745aa04ac117fe6fec6fb8b319adeeadef3
samahmajadla <samahmajadla@gmail.com>
2018-04-28 16:03:59 -0400

    added receipt, tip, total, in order

--- src/Order.java
+++ src/Order.java
@@ -1,15 +1,51 @@
+import java.time.LocalDateTime;
 import java.util.LinkedList;
 
 public class Order {
 
-    private LinkedList<ConsumableItem> items;
-    public Order(){
-        items = new LinkedList<>();
+
+    private class Receipt {
+        private static final double TAX_RATE = 0.06;
+        private double tipAmount;
+        private double subtotal = 0;
+
+        Receipt() {
+            this.tipAmount = 0;
+        }
+
+        private double getTipAmount() {
+            return tipAmount;
+        }
+
+        private double getSubtotal() {
+            return subtotal; }
+
+        private void addToSubtotal(double priceToAdd) {
+            this.subtotal += priceToAdd;
+        }
+
+        public void setTipAmount(double tipAmount) {
+            this.tipAmount = tipAmount;
+        }
     }
 
 
+    private LinkedList<ConsumableItem> items;
+    private String customerName;
+    private  LocalDateTime time;
+    private Receipt receipt;
+    private boolean tacoPriceBreakFlag = false;
+
+    public Order( String customerName ){
+        time = LocalDateTime.now();
+        this.customerName = customerName;
+        items = new LinkedList<>();
+        receipt = new Receipt();
+    }
+
     public void addItem(ConsumableItem item) {
         items.add(item);
+        receipt.addToSubtotal(item.getUnitPrice());
     }
 
     public int getOrderSize() {
@@ -17,7 +53,54 @@ public class Order {
     }
 
 
+
     public ConsumableItem getItem(int index) {
         return items.get(index);
+    }
+
+    public String getCustomerName() {
+        return this.customerName;
+    }
+
+    public LocalDateTime getOrderTime() {
+        return time;
+    }
+
+    public double getTaxRate() {
+        return Receipt.TAX_RATE;
+    }
+
+    public double getTipAmount() {
+        return this.receipt.getTipAmount();
+    }
+
+    public void setTipAmount(double tipAmount) {
+        this.receipt.setTipAmount( tipAmount );
+    }
+
+    public double getSubtotal() {
+        return this.receipt.getSubtotal();
+    }
+
+    public double getDiscountAmount() {
+        int tacoCount = this.getTacoCount();
+        if( tacoCount >= 3 ) {
+            return 0.5 * tacoCount;
+        }
+        return 0;
+    }
+
+    private int getTacoCount() {
+            int count = 0;
+            for (ConsumableItem element : this.items) {
+                if (element instanceof Taco) {
+                    count++;
+                }
+            }
+            return count;
+    }
+
+    public double getTotal() {
+        return getSubtotal() * (1+getTaxRate()) - getDiscountAmount();
     }
 }
--- test/OrderTest.java
+++ test/OrderTest.java
@@ -1,6 +1,8 @@
 import org.junit.Before;
 import org.junit.Test;
 
+import java.time.LocalDateTime;
+
 import static org.junit.Assert.*;
 
 public class OrderTest {
@@ -9,7 +11,7 @@ public class OrderTest {
 
     @Before
     public void setUp() {
-      order = new Order();
+      order = new Order("customer name");
     }
 
     @Test
@@ -48,8 +50,97 @@ public class OrderTest {
 
     @Test
     public void testOrderDefaults() {
-
         assertEquals(0, order.getOrderSize());
-
     }
+
+    @Test
+    public void testOrderHasName() {
+        Order namedOrder = new Order("Sam");
+        assertEquals("Sam", namedOrder.getCustomerName());
+        assertEquals("customer name", order.getCustomerName());
+    }
+
+    @Test
+    public void testOrderHasDateStamp(){
+        LocalDateTime time = LocalDateTime.now();
+
+        LocalDateTime orderTime = order.getOrderTime();
+        assertEquals(time.getDayOfYear() , orderTime.getDayOfYear());
+        assertEquals(time.getHour() , orderTime.getHour());
+        assertEquals(time.getMinute() , orderTime.getMinute());
+    }
+
+    @Test
+    public void testReceiptDefaults() {
+        assertEquals(0.06, order.getTaxRate(),0);
+        assertEquals(0.0, order.getTipAmount(),0);
+        assertEquals(0.0, order.getSubtotal(), 0 );
+    }
+
+    @Test
+    public void testAddedItemsAddToTotal(){
+        order.addItem(new Quesadilla());
+        assertEquals(5, order.getSubtotal(),0);
+    }
+
+    @Test
+    public void testAdding3TacosGivesPriceBreak() {
+        Taco taco = new Taco();
+        order.addItem(taco);
+        order.addItem(taco);
+        order.addItem(taco);
+        assertEquals(1.50, order.getDiscountAmount(), 0);
+    }
+
+    @Test
+    public void testAdding2TacosGivesNoPriceBreak() {
+        Taco taco = new Taco();
+        order.addItem(taco);
+        order.addItem(taco);
+        order.addItem(new Coke());
+        assertEquals(0, order.getDiscountAmount(), 0);
+    }
+
+    @Test
+    public void testTotal_givenFourTacos_andADeliciousHorchata_hasDiscountAndTax(){
+        double expectedTotal = (((4 * 3) + 2.5) * 1.06) - (4 * .5);
+
+        Taco taco = new Taco();
+        order.addItem(taco);
+        order.addItem(taco);
+        order.addItem(taco);
+        order.addItem(taco);
+
+        order.addItem(new Horchata());
+
+        assertEquals(expectedTotal, order.getTotal(), 0);
+    }
+
+    @Test
+    public void testTotal_givenTwoTacos_andACoke_hasNoDiscountYetTax() {
+        double expectedTotal = ((2 *3) + 1.75) * 1.06;
+        Taco taco = new Taco();
+
+       order.addItem(taco);
+       order.addItem(taco);
+       order.addItem(new Coke());
+
+       assertEquals(expectedTotal, order.getTotal(),0);
+    }
+
+    @Test
+    public void testTipAmountCorrectlyAdded() {
+        Taco taco = new Taco();
+        Quesadilla quesadilla = new Quesadilla();
+
+        order.addItem(taco);
+        order.addItem(quesadilla);
+    }
+
+    @Test
+    public void testTipSetter(){
+        order.setTipAmount(2);
+        assertEquals(2,order.getTipAmount(),0);
+    }
+
 }
